fa2efc7e954fa3a3afe91ce2024670115de78d7f
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-11 02:45:53 -0400

    Added registry search API stub.

--- api/searchCorporations.js
+++ api/searchCorporations.js
@@ -1,8 +1,23 @@
 import Companies from '../data/companies'
+import fetch from 'node-fetch'
 
 export default async (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
+    const url = 'https://searchapi.mrasservice.ca/Search/api/v1/search'
+    const keyword = req.query.q
+    const params = new URLSearchParams({
+        fq: `keyword:"${keyword}" Status_State:Active`,
+        location: req.query.location || 'ON',
+        page: req.query.page || 0,
+        lang: req.query.lang || 'en',
+        sortfield: 'score',
+        sortorder: 'desc',
+        queryaction: 'fieldquery',
+    })
 
-    res.send(JSON.stringify(Companies.docs, null, 2))
+    console.log(`${url}?${params}`)
+    const response = await fetch(`${url}?${params}`)
+    const json = await response.json()
+    res.send(JSON.stringify(json, null, 2))
 }
