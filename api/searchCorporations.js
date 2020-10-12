37bca790ac343c8dc0d157d04408143ae7ceeec1
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-11 22:01:42 -0400

    Made api paging start at 1. Set proper casing on location.

--- api/searchCorporations.js
+++ api/searchCorporations.js
@@ -5,19 +5,20 @@ import { filterCompanies } from './utils'
 export default async (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    const url = 'https://searchapi.mrasservice.ca/Search/api/v1/search'
+    const apiBaseUrl = 'https://searchapi.mrasservice.ca/Search/api/v1'
+    const apiPath = '/search'
     const keyword = req.query.q
     const params = new URLSearchParams({
         fq: `keyword:"${keyword}" Status_State:Active`,
-        location: req.query.location || 'ON',
-        page: req.query.page || 0,
+        location: (req.query.location || 'ON').toUpperCase(),
+        page: (req.query.page || 1) - 1,
         lang: req.query.lang || 'en',
         sortfield: 'score',
         sortorder: 'desc',
         queryaction: 'fieldquery',
     })
 
-    const response = await fetch(`${url}?${params}`)
+    const response = await fetch(`${apiBaseUrl}${apiPath}?${params}`)
     const json = await response.json()
     let entries = json.docs
     entries = entries.filter(e => e.Registry_Source === params.get('location'))
