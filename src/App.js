4ff344108230156c27462eeb1bacda0bf973a8e3
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-14 23:09:48 -0400

    Added react-router support.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Switch, Redirect, Route, BrowserRouter as Router } from 'react-router-dom';
 import { Wizard } from './components/Wizard'
 
 const App = () => (
-  <Wizard />
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/new" />
+      </Route>
+      <Route path="/new">
+        <Wizard />
+      </Route>
+    </Switch>
+  </Router>
 )
 
 export default App;
