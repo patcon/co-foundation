189c7784d5352d9d490ad51d196293d283f9b3a2
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-17 17:21:41 -0400

    Added director and officer pages.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,101 @@
-import { Button, Pane, Paragraph, Tablist, SidebarTab, Heading, Link, majorScale, SelectField } from 'evergreen-ui'
+import { Button, Text, Pane, Paragraph, Tablist, SidebarTab, Heading, Link, majorScale, SelectField, PlusIcon } from 'evergreen-ui'
 import React, { useState } from 'react'
 import { Link as RouterLink, Switch, Route, Redirect } from 'react-router-dom'
 import useToggle from '../hooks/useToggle'
 import { FakeLoadButton } from './Wizard'
+
+const HotDogCards = () => (
+  <>
+    <Pane>
+      <Text>Name</Text>
+    </Pane>
+    <Pane>
+      <HotDogCard />
+    </Pane>
+  </>
+)
+
+const HotDogCard = () => {
+  const role = 'President'
+  const otherRoles = ['Director', 'Shareholder', 'Secretary']
+  return (
+    <Pane>
+      <Pane padding={majorScale(1)} elevation={2}><Text>Patrick Connolly</Text></Pane>
+      <Pane>
+        <Pane elevation={1} marginX={majorScale(1)}>
+          <Pane display="flex" justifyContent="flex-end" alignItems="center" marginX={majorScale(3)} height={majorScale(6)}>
+            <Pane>
+              <Link marginLeft={majorScale(3)} cursor="pointer">Edit Contact Info</Link>
+              <Link marginLeft={majorScale(3)} cursor="pointer">Remove</Link>
+              <Link marginLeft={majorScale(3)} cursor="not-allowed">Replace</Link>
+            </Pane>
+          </Pane>
+          <Pane padding={majorScale(3)}>
+            <Pane>
+              <Pane marginBottom={majorScale(3)}>
+                <Text>
+                  This {role} in also a {otherRoles.join(', ')}.
+                </Text>
+              </Pane>
+              <Pane>
+                <Paragraph>me@example.com</Paragraph>
+                <Paragraph>555-555-5555</Paragraph>
+                <Paragraph>123 Any Street, Unit 3, Anytown, ON, Canada, H0H 0H0</Paragraph>
+              </Pane>
+            </Pane>
+            <Paragraph textAlign="right">Canadian Resident</Paragraph>
+          </Pane>
+        </Pane>
+      </Pane>
+    </Pane>
+  )
+}
+
+const DirectorsPage = () => {
+  return (
+    <AppPage
+      title="Directors"
+      byline="The Directors appoint Officers and oversee management of the company."
+      moreLink="https://get.founded.help/roles-in-your-corporation/what-is-a-director"
+    >
+      <Pane display="flex" justifyContent="flex-end">
+        <Button appearance="primary" textTransform="uppercase">Add Director <PlusIcon /> </Button>
+      </Pane>
+      <Pane>
+        <HotDogCards />
+      </Pane>
+    </AppPage>
+  )
+}
+
+const OfficersPage = () => {
+  return (
+    <AppPage
+      title="Officers"
+      byline="Officers actively operate and manage the business. President and Secretary are mandatory however companies can create other positions like CEO, Treasurer, etc. These can all be held by the same person."
+      moreLink="https://get.founded.help/roles-in-your-corporation/what-is-an-officer"
+    >
+      <Pane display="flex" justifyContent="flex-end">
+        <Button appearance="primary" textTransform="uppercase">Add Officer <PlusIcon /> </Button>
+      </Pane>
+      <Pane>
+        <Pane>
+          <Heading is="h4" size={700}>President</Heading>
+          <Pane>
+            <HotDogCards />
+          </Pane>
+        </Pane>
+        <hr style={{margin: '36px 0'}} />
+        <Pane>
+          <Heading is="h4" size={700}>Secretary</Heading>
+          <Pane>
+            <HotDogCards />
+          </Pane>
+        </Pane>
+      </Pane>
+    </AppPage>
+  )
+}
 
 export const Dashboard = props => {
   const { path } = props.match
@@ -10,6 +103,8 @@ export const Dashboard = props => {
   const tabs = [
     {name: 'Dashboard', component: <AppPage title="Dashboard" />},
     {name: 'Organization', component: <CompanyInfoPage />},
+    {name: 'Directors', component: <DirectorsPage />},
+    {name: 'Officers', component: <OfficersPage />},
     {name: 'Shares', component: <AppPage title="Shares" />},
     {name: 'Employees', component: <AppPage title="Employees" />},
     {name: 'Documents', component: <AppPage title="Documents" />},
@@ -235,15 +330,15 @@ CompanyInfoBox.defaultProps = {
 }
 
 export const AppPage = props => {
-  const { title, byline, more_link, children } = props
+  const { title, byline, moreLink, children } = props
   return (
     <Pane>
       <Pane>
         <Heading size={900} textTransform="uppercase" is="h1">{title}</Heading>
         <Heading is="h5">
-          {byline}
-          {more_link
-            ? <Link target="_blank" href={more_link}>Read More</Link>
+          {byline + ' '}
+          {moreLink
+            ? <Link target="_blank" href={moreLink}>Read More</Link>
             : null
           }
         </Heading>
